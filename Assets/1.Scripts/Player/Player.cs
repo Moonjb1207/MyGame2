60659ebb7f8be7a7732048898590cb1337153d43
Moon <postick1207@gmail.com>
2023-12-26 13:29:35 +0900

    23_12_26
    lesson_1

---
+++ Assets/1.Scripts/Player/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;
+    public Rigidbody rg;
+    public int curHP;
+
+
+    private void Awake()
+    {
+        rg = GetComponent<Rigidbody>();
+        curHP = 10;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
+            MoveTo(Vector3.forward);
+        }
+    }
+
+    public void MoveTo(Vector3 dir)
+    {
+        rg.velocity = dir * moveSpeed;
+    }
+}
