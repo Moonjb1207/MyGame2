6bd2e579a4bbd42cf2ecdbf670e8b9a2445a4e4b
Moon <postick1207@gmail.com>
2024-01-05 11:12:38 +0900

    24_01_05
    lesson_4

---
+++ Assets/1.Scripts/UI/TapBtn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapBtn : MonoBehaviour
+{
+    public int idx;
+    Button btn;
+    private void Awake()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(OnClickedTapBtn); //코드로 Button 컴포넌트에 함수 등록 
+    }
+
+    void OnClickedTapBtn()
+    {
+        GetComponentInParent<TapContainer>().Select(idx);
+    }
+}
