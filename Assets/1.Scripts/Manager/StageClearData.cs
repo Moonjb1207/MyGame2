ba5f9572147a0ddb3b36a6bfb6d4e4c7172896e4
Moon <postick1207@gmail.com>
2024-03-25 03:16:57 +0900

    StageClearData script readme add
    주의사항 주석 추가

--- Assets/1.Scripts/Manager/StageClearData.cs
+++ Assets/1.Scripts/Manager/StageClearData.cs
@@ -3,7 +3,11 @@ using System;
 [Serializable]
 public class StageClearData
 {
+    //스테이지 락, 구조물 저장 배열은 추가할때마다 늘려줘야함
+
+    //튜토리얼 0, 무한모드 Length - 1
     public bool[] isUnlock = new bool[4];
     public int Gold;
+    //구조물이름_레벨
     public string[] buildingLevel = new string[4];
 }
